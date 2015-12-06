d241bf5187fbd63f2a543d7317be1ef92155f4fb
Daniel Berger <djberg96@gmail.com>
2015-12-05 17:43:04 -0700

    Version bump.

--- lib/union.rb
+++ lib/union.rb
@@ -3,7 +3,7 @@
 #
 class Union < Struct
   # The version of the union library
-  VERSION = '1.0.4'
+  VERSION = '1.0.5'
 
   # Creates and returns a new Union. Unlike Struct::Class.new, this does not
   # take any arguments. You must assign attributes individually.
