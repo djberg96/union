003c5d666bd09b7cd2b03fbba59511db90f2ade7
Daniel Berger <djberg96@gmail.com>
2015-12-05 17:44:58 -0700

    Version test bump.

--- test/test_union.rb
+++ test/test_union.rb
@@ -6,20 +6,20 @@ class TC_Union < Test::Unit::TestCase
     Union.new('Human', :name, :age, :height) unless defined? Union::Human
     @union = Union::Human.new
   end
-   
+
   def test_union_version
-    assert_equal('1.0.4', Union::VERSION)
+    assert_equal('1.0.5', Union::VERSION)
   end
-   
-  def test_union_constructor      
+
+  def test_union_constructor
     assert_raise(ArgumentError){ Union::Human.new('Matz') }
   end
-   
+
   def test_union_attribute_assignment_basic
     assert_nothing_raised{ @union.name = 'Daniel' }
     assert_equal('Daniel', @union.name)
   end
-   
+
   def test_union_attribute_assignment_by_method_name
     assert_nothing_raised{ @union.name = 'Daniel' }
     assert_nothing_raised{ @union.age = 38 }
@@ -27,7 +27,7 @@ class TC_Union < Test::Unit::TestCase
     assert_nil(@union.height)
     assert_equal(38, @union.age)
   end
-   
+
   def test_union_attribute_assignment_by_string_ref
     assert_nothing_raised{ @union['name'] = 'Daniel' }
     assert_nothing_raised{ @union['age'] = 38 }
@@ -35,15 +35,15 @@ class TC_Union < Test::Unit::TestCase
     assert_nil(@union['height'])
     assert_equal(38, @union['age'])
   end
-   
+
   def test_union_attribute_assignment_by_symbol_ref
     assert_nothing_raised{ @union[:name] = 'Daniel' }
     assert_nothing_raised{ @union[:age] = 38 }
     assert_nil(@union[:name])
     assert_nil(@union[:height])
     assert_equal(38, @union[:age])
-  end         
-   
+  end
+
   def teardown
     @union = nil
   end
