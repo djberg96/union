9ff203b19ef072090c1a5babfcc1157a549a5d1b
Daniel Berger <djberg96@gmail.com>
2018-11-04 09:33:54 -0700

    Update and freeze VERSION constant.

--- lib/union.rb
+++ lib/union.rb
@@ -3,7 +3,7 @@
 #
 class Union < Struct
   # The version of the union library
-  VERSION = '1.0.5'
+  VERSION = '1.0.6'.freeze
 
   # Creates and returns a new Union. Unlike Struct::Class.new, this does not
   # take any arguments. You must assign attributes individually.
